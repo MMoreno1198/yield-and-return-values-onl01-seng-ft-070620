56143c99ac09d5f7dd604351d0ba3b0efe97cb6b
Marcus Moreno <legendary.1198@gmail.com>
2020-07-10 04:45:36 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,20 +1,16 @@
-def hello(array)
-  
-  i = 0
+  require 'pry'
 
-  collection = []
+def hello(array)
  
+  i = 0
+  
   while i < array.length
   
     yield(array[i])
   
-   collection << yield(array[i])
-  
     i += 1
  
   end
- 
-  collection
 
 end
 
